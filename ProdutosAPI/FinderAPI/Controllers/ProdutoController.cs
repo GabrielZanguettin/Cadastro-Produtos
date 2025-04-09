a77025315d517020b38c23091d9be04179acf96f
Gabriel Zanguettin Costa <gabrielzanguettintecnologia@gmail.com>
2025-04-09 15:18:21 -0300

    Add files via upload

---
+++ ProdutosAPI/FinderAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,56 @@
+﻿using FinderAPI.Entidades;
+using FinderAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinderAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoService produtoService;
+        public ProdutoController(ProdutoService produtoService)
+        {
+            this.produtoService = produtoService;
+        }
+        [HttpGet("produtos/count")]
+        public async Task<IActionResult> GetCountProdutos()
+        {
+            var total = await produtoService.GetProductsCount();
+            return Ok(total);
+        }
+        [HttpGet("produtos")]
+        public async Task<IActionResult> GetProdutosDoBanco(int page, int pageSize)
+        {
+            try
+            {
+                var produtos = await produtoService.GetProdutos(page, pageSize);
+                return Ok(produtos);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { message = "Erro ao encontrar produtos", error = ex.Message });
+            }
+        }
+        [HttpPost("produto")]
+        public async Task<IActionResult> RegistrarProduto([FromBody] Produto produto)
+        {
+            try
+            {
+                var produtoCadastrado = await produtoService.RegistrarProdutoNoBanco(produto);
+                return Ok(produtoCadastrado);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+        [HttpPost("produtos")]
+        public async Task<IActionResult> RegistrarProdutos([FromBody] List<Produto> produtos)
+        {
+            var produtosCadastrados = await produtoService.CadastrarProdutos(produtos);
+            return Ok(produtosCadastrados);
+        }
+    }
+}
---
+++ ProdutosAPI/FinderAPI/Services/ProdutoService.cs
@@ -0,0 +1,45 @@
+﻿using FinderAPI.Entidades;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace FinderAPI.Services
+{
+    public class ProdutoService
+    {
+        private readonly ISessionFactory sessionFactory;
+        public ProdutoService(ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+        public async Task<Produto> RegistrarProdutoNoBanco(Produto produto)
+        {
+            using var session = sessionFactory.OpenSession();
+            using var transaction = session.BeginTransaction();
+            await session.SaveAsync(produto);
+            await transaction.CommitAsync();
+            return produto;
+        }
+        public async Task<List<Produto>> CadastrarProdutos(List<Produto> produtos)
+        {
+            var produtosCadastrados = new List<Produto>();
+            foreach (var produto in produtos)
+            {
+                var produtoCadastrado = await RegistrarProdutoNoBanco(produto);
+                produtosCadastrados.Add(produtoCadastrado);
+            }
+            return produtosCadastrados;
+        }
+        public async Task<List<Produto>> GetProdutos(int page, int pageSize) {
+            using var session = sessionFactory.OpenSession();
+            var products = page == 0 ? await session.Query<Produto>().ToListAsync() : 
+                await session.Query<Produto>().Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+            return products ?? new List<Produto>();
+        }
+        public async Task<int> GetProductsCount()
+        {
+            using var session = sessionFactory.OpenSession();
+            var total = await session.Query<Produto>().CountAsync();
+            return total;
+        }
+    }
+}
